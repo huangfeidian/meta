f2c820e32c0a6a1e7dd6c73691e2fdacfa15229b
huangfeidian <huangfeidian@live.cn>
2019-06-11 00:25:48 +0800

    准备dump所有信息

--- include/nodes/type_info.h
+++ include/nodes/type_info.h
@@ -1,14 +1,16 @@
 ﻿#pragma once
 
+#include <nlohmann/json.hpp>
 #include "node_base.h"
 #include "../name_space.h"
 namespace meta::language
 {
+	using json = nlohmann::json;
     class type_info
     {
     public:
         
-        const node* base_type() const; // return the base type example: int vector
+        const type_info* ref_type() const; // return the base type example: int vector
         const std::vector<const type_info*>& template_args() const;
         bool is_completed() const; // is this a type that depends on other variable not in current lexical scope
         bool is_templated() const;// is this a template type
@@ -19,11 +21,17 @@ namespace meta::language
         bool is_reference() const; // is reference
 		bool is_lvalue_refer() const;// is &
 		bool is_rvalue_refer() const;// is &&
-		enum CXTypeKind get_kind() const;
-		const std::string& qualified_name() const;
+		bool is_pointer() const; // is T* const T*
+		const type_info* point_to() const;// return T for T * return const T for const T * else nullptr
+		const type_info* refer_to() const;// return (const) T for (const) T &/&& return nullptr for others
+		enum CXTypeKind kind() const;
+		const CXType& type() const;
+		std::string qualified_name() const;
 		const std::string& name() const;
 		bool has_type() const;
+		json to_json() const;
     private:
+
 		void set_type(CXType _in_type);
         type_info(const std::string& _in_name, const CXType& _in_type, const type_info* _in_ref_type);
 		type_info(const std::string& _in_name, enum CXTypeKind _in_kind);
@@ -31,7 +39,7 @@ namespace meta::language
 		CXType _type;
 		const std::string _name;
 		enum CXTypeKind _kind;
-		const type_info* ref_type;
+		const type_info* _ref_type;
         friend class type_db;
 	};
     class type_db
@@ -46,9 +54,9 @@ namespace meta::language
         }
         void clear_all_type_info();
     private:
-		type_info* get_type_for_const(const CXType& in_type);
-		type_info* get_type_for_pointee(const CXType& in_type);
-		type_info* get_type_for_template(const CXType& in_type);
+		type_info* get_type_for_const(const CXType& _in_type);
+		type_info* get_type_for_pointee(const CXType& _in_type);
+		type_info* get_type_for_template(const CXType& _in_type);
         std::unordered_map<std::string, type_info*> _type_data; // qualitified name to type_info
         type_db();
 
--- src/type_info.cpp
+++ src/type_info.cpp
@@ -28,7 +28,7 @@ namespace meta::language
 {
 	type_info::type_info(const std::string& _in_name, enum CXTypeKind _in_kind):
 		_kind(_in_kind),
-		ref_type(nullptr),
+		_ref_type(nullptr),
 		_name(_in_name)
 	{
 
@@ -36,11 +36,132 @@ namespace meta::language
 	type_info::type_info(const std::string& _in_name, const CXType& _in_type, const type_info* _in_ref_type):
 		_type(_in_type),
 		_kind(_in_type.kind),
-		ref_type(_in_ref_type),
+		_ref_type(_in_ref_type),
 		_name(_in_name)
 	{
 
 	}
+	const std::string& type_info::name() const
+	{
+		return _name;
+	}
+	std::string type_info::qualified_name() const
+	{
+		auto the_cursor = clang_getTypeDeclaration(_type);
+		return utils::get_qualified_name_from_cursor(the_cursor);
+	}
+	const CXType& type_info::type() const
+	{
+		return _type;
+	}
+	enum CXTypeKind type_info::kind() const
+	{
+		return _kind;
+	}
+	const type_info* type_info::ref_type() const
+	{
+		return _ref_type;
+	}
+	void type_info::set_type(CXType _in_type)
+	{
+		_type = _in_type;
+		_kind = _type.kind;
+	}
+	const std::vector<const type_info*>& type_info::template_args() const
+	{
+		return _template_args;
+	}
+	bool type_info::is_completed() const
+	{
+		if (_kind == CXTypeKind::CXType_Unexposed)
+		{
+			auto decl_type = clang_getTypeDeclaration(_type);
+			if (decl_type.kind == CXTypeKind::CXType_Invalid)
+			{
+				return false;
+			}
+			return true;
+		}
+		if (_template_args.empty())
+		{
+			return true;
+		}
+		for (auto i : _template_args)
+		{
+			if (!i->is_completed())
+			{
+				return false;
+			}
+
+		}
+		return true;
+	}
+	bool type_info::is_templated() const
+	{
+		return !_template_args.empty();
+	}
+	bool type_info::is_callable() const
+	{
+		return false;
+	}
+	bool type_info::is_const() const
+	{
+		return clang_isConstQualifiedType(_type);
+	}
+	bool type_info::is_reference() const
+	{
+		return _kind == CXTypeKind::CXType_LValueReference || _kind == CXTypeKind::CXType_RValueReference;
+	}
+	bool type_info::is_lvalue_refer() const
+	{
+		return _kind == CXTypeKind::CXType_LValueReference;
+	}
+	bool type_info::is_rvalue_refer() const
+	{
+		return _kind == CXTypeKind::CXType_RValueReference;
+	}
+	bool type_info::is_pointer() const
+	{
+		return _kind == CXTypeKind::CXType_Pointer || _kind == CXTypeKind::CXType_BlockPointer;
+	}
+	const type_info* type_info::point_to() const
+	{
+		if (!is_pointer())
+		{
+			return nullptr;
+		}
+		return _ref_type;
+	}
+	const type_info* type_info::refer_to() const
+	{
+		if (!is_reference())
+		{
+			return nullptr;
+		}
+		return _ref_type;
+	}
+	json type_info::to_json() const
+	{
+		json result;
+		result["name"] = _name;
+		result["qualified_name"] = qualified_name();
+		result["kind"] = static_cast<std::uint32_t>(_kind);
+		if (_ref_type)
+		{
+			result["ref_type"] = _ref_type->qualified_name();
+		}
+		if (!_template_args.empty())
+		{
+			std::vector<std::string> args;
+			for (const auto i : _template_args)
+			{
+				args.push_back(i->qualified_name());
+			}
+			result["args"] = args;
+		}
+		return result;
+	}
+
 	type_info* type_db::get_type_for_const(const CXType& _in_type)
 	{
 		auto full_name = utils::to_string(_in_type);
@@ -73,7 +194,7 @@ namespace meta::language
 			}
 			else
 			{
-				// some error case I cant handle
+				
 				auto pointer_to_type = clang_getPointeeType(_in_type);
 				if (pointer_to_type.kind != CXTypeKind::CXType_Invalid)
 				{
@@ -85,17 +206,26 @@ namespace meta::language
 					auto temp_iter = _type_data.find(pointer_to_type_name);
 					if (temp_iter == _type_data.end())
 					{
-						auto temp_result = new type_info(full_name, _in_type, pointee_type);
+						temp_result = new type_info(pointer_to_type_name, _in_type, pointee_type);
 					}
-
+					else
+					{
+						temp_result = temp_iter->second;
+					}
+					auto final_result = new type_info(full_name, _in_type, temp_result);
 					
 					_type_data[full_name] = final_result;
 					return final_result;
 
 				}
-				auto final_result = new type_info(full_name, _in_type, nullptr);
-				_type_data[full_name] = final_result;
-				return final_result;
+				else
+				{
+					// some error case I cant handle
+					auto final_result = new type_info(full_name, _in_type, nullptr);
+					_type_data[full_name] = final_result;
+					return final_result;
+				}
+
 			}
 		}
 		else
@@ -108,17 +238,46 @@ namespace meta::language
 			return final_type;
 		}
 	}
+	type_info* type_db::get_type_for_pointee(const CXType& _in_type)
+	{
+		auto full_name = utils::to_string(_in_type);
+		auto pointer_to_type = clang_getPointeeType(_in_type);
+		auto pointee_type = get_type(pointer_to_type);
+		auto final_result = new type_info(full_name, _in_type, pointee_type);
+		_type_data[full_name] = final_result;
+		return final_result;
+	}
+	type_info* type_db::get_type_for_template(const CXType& _in_type)
+	{
+		auto full_name = utils::to_string(_in_type);
+		auto argu_num = clang_Type_getNumTemplateArguments(_in_type);
+		auto base_type = get_type(clang_getTypeDeclaration(_in_type));
+		std::vector<type_info*> arg_types;
+		for (int i = 0; i < argu_num; i++)
+		{
+			auto temp_arg_type = get_type(clang_Type_getTemplateArgumentAsType(_in_type, i));
+			arg_types.push_back(temp_arg_type);
+
+		}
+		auto final_result = new type_info(full_name, _in_type, base_type);
+		_type_data[full_name] = final_result;
+		return final_result;
+	}
+
 	type_info* type_db::get_type(const CXType& _in_type)
 	{
 		auto full_name = utils::to_string(_in_type);
 		auto type_iter = _type_data.find(full_name);
 		if (type_iter != _type_data.end())
 		{
+			auto result = type_iter->second;
+			if (result->_type.kind != CXTypeKind::CXType_Invalid)
+			{
+				result->set_type(_in_type);
+			}
 			return type_iter->second;
 		}
 		auto is_const = clang_isConstQualifiedType(_in_type);
-		auto cur_kind = _in_type.kind;
-		type_info* result;
 		if (is_const)
 		{
 			return get_type_for_const(_in_type);
@@ -129,13 +288,17 @@ namespace meta::language
 		{
 			// is a reference type but not a const T* const
 			// is not a reference type
+			return get_type_for_pointee(_in_type);
 
 		}
-		else
+		auto argu_num = clang_Type_getNumTemplateArguments(_in_type);
+		if (argu_num > 0)
 		{
-			result = get_type(pointer_to_type);
+			return get_type_for_template(_in_type);
 		}
 
-		return result;
+		auto final_type = new type_info(full_name, _in_type, nullptr);
+		_type_data[full_name] = final_type;
+		return final_type;
 	}
 }
