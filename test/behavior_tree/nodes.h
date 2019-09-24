33b434a93a881d57fb2085591632f34e12f7e504
huangfeidian <huangfeidian@live.cn>
2019-09-24 21:15:02 +0800

    完善nodes的创建

--- test/behavior_tree/nodes.h
+++ test/behavior_tree/nodes.h
@@ -7,17 +7,19 @@ namespace behavior
 	enum class node_type
 	{
 		invalid = 0,
-		node_root,
-		node_not,
-		node_true,
-		node_sequence,
-		node_always_seq,
-		node_if_else,
-		node_while,
-		node_wait_event,
-		node_reset,
-		node_sub_tree,
-		node_parallel
+		root,
+		negative,
+		sequence,
+		always_seq,
+		random_seq,
+		if_else,
+		while_loop,
+		wait_event,
+		reset,
+		sub_tree,
+		parallel,
+		action,
+		always_true
 	};
 	enum class node_state
 	{
@@ -57,6 +59,7 @@ namespace behavior
 
 		}
 	};
+	class node_creator;
 	class node
 	{
 	protected:
@@ -79,7 +82,8 @@ namespace behavior
 		std::shared_ptr<node_closure> _closure;
 		std::shared_ptr<spdlog::logger> _logger;
 
-		node(node* in_parent, node_idx_type in_node_idx, const btree_desc& in_btree, node_type in_type) :
+		node(node* in_parent, node_idx_type in_node_idx, 
+			const btree_desc& in_btree, node_type in_type) :
 			_parent(in_parent),
 			_node_idx(in_node_idx),
 			btree_config(in_btree),
@@ -100,18 +104,7 @@ namespace behavior
 		{
 			return _state == node_state::leaving || _state == node_state::dead;
 		}
-		void set_result(bool new_result)
-		{
-			if (node_state_is_forbid_enter())
-			{
-				_logger->warn("current state is {} while set result {} at node {}", int(_state), new_result, node_config.idx);
-				_agent->notify_stop();
-				return;
-			}
-			result = new_result;
-			_state = node_state::dead;
-			backtrace();
-		}
+		void set_result(bool new_result);
 		virtual bool handle_event(const event_type& cur_event)
 		{
 			return false;
@@ -120,592 +113,223 @@ namespace behavior
 		{
 			return _ready;
 		}
-		void visit()
-		{
-			
-			switch (_state)
-			{
-			case node_state::init:
-				create_children();
-				on_enter();
-				if (_state == node_state::entering)
-				{
-					_logger->warn("btree {} on_enter node {} while after state {}", 
-						btree_config.tree_name, node_config.idx, int(_state));
-					_agent->notify_stop();
-				}
-				break;
-			case node_state::awaken:
-				on_revisit();
-				if (_state == node_state::revisiting)
-				{
-					_logger->warn("btree {} revisit node {} while after state {}",
-						btree_config.tree_name, node_config.idx, int(_state));
-					_agent->notify_stop();
-				}
-				break;
-			
-			default:
-				_logger->warn("btree {} visit node {} with invalid state {}",
-					btree_config.tree_name, node_config.idx, int(_state));
-				_agent->notify_stop();
-				break;
-			}
-
-
-		}
-		void create_children()
-		{
-			if (!node_config.children.empty() && children.empty())
-			{
-				// 初始化所有的子节点
-				for (auto one_child_idx : node_config.children)
-				{
-					children.push_back(create_node_by_idx(btree_config, one_child_idx, this));
-				}
-			}
-		}
-		virtual void on_enter()
-		{
-			_state = node_state::entering;
-			next_child_idx = 0;
-			result = false;
-			for (auto one_child : children)
-			{
-				one_child->_state = node_state::init;
-			}
-		}
-		virtual void on_revisit()
-		{
-			_state = node_state::revisiting;
-		}
-		virtual void visit_child(node_idx_type child_idx)
-		{
-			if (child_idx >= children.size())
-			{
-				_logger->warn("btree {} visit child {} at node {} while children size is {}",
-					btree_config.tree_name, child_idx, node_config.idx, children.size());
-				_agent->notify_stop();
-				return;
-			}
-			children[child_idx]->_state = node_state::init;
-			_agent->_fronts.push_back(children[child_idx]);
-			_state = node_state::wait_child;
-
-		}
-		virtual void leave()
-		{
-			_closure.reset();
-			_state = node_state::leaving;
-		}
-		virtual void interrupt()
-		{
-			if (_closure)
-			{
-				_closure.reset();
-			}
-			_state = node_state::dead;
-			next_child_idx = 0;
-		}
-		virtual void backtrace()
-		{
-			if (_parent)
-			{
-				_agent->_fronts.push_back(_parent);
-				_parent->_state = node_state::awaken;
-			}
-			else
-			{
-				_agent->_fronts.push_back(this);
-				_state = node_state::awaken;
-			}
-		}
-		const std::string& tree_name() const
-		{
-			return btree_config.tree_name;
-		}
-		void create_sub_node()
-		{
-
-		}
-		std::string debug_info() const
-		{
-			return fmt::format("btree {} node {} child_idx {} extra {}", 
-				btree_config.tree_name, node_config.idx, next_child_idx, meta::serialize::encode(node_config.extra));
-		}
+		void visit();
+		void create_children();
+		virtual void on_enter();
+		virtual void on_revisit();
+		virtual void visit_child(node_idx_type child_idx);
+		virtual void leave();
+		virtual void interrupt();
+		virtual void backtrace();
+		const std::string& tree_name() const;
+		std::string debug_info() const;
+		friend class node_creator;
 	};
 
 	
-	static node* create_node_by_idx(const btree_desc& btree_config, node_idx_type node_idx, node* parent);
+	
 	class root :public node
 	{
-	public:
-		void on_enter()
+		root(node* in_parent, node_idx_type in_node_idx, 
+			const btree_desc& in_btree, node_type in_type) :
+			node(in_parent, in_node_idx, in_btree, in_type)
 		{
-			node::on_enter();
-			visit_child(0);
+
 		}
-		void on_revisit()
-		{
-			node::on_revisit();
-			if (!_parent)
-			{
-				on_enter();
-			}
-			else
-			{
-				bool result = children[0]->result;
-				set_result(result);
-			}
-			
-		}
+	private:
+		void on_enter();
+		void on_revisit();
+		friend class node_creator;
 	};
 	class sequence :public node
 	{
-		void on_enter()
+	protected:
+		sequence(node* in_parent, node_idx_type in_node_idx, 
+			const btree_desc& in_btree, node_type in_type) :
+			node(in_parent, in_node_idx, in_btree, in_type)
 		{
-			node::on_enter();
-			next_child_idx = 0;
-			visit_child(0);
-		}
-		void on_revisit()
-		{
-			node::on_revisit();
-			if (!children[next_child_idx]->result)
-			{
-				set_result(false);
-				return;
-			}
-			next_child_idx += 1;
-			if (next_child_idx == children.size())
-			{
-				set_result(true);
-				return;
-			}
-			visit_child(next_child_idx);
-		}
-	};
-	class always_sequence :public sequence
-	{
-		void on_revisit()
-		{
-			node::on_revisit();
 
-			next_child_idx += 1;
-			if (next_child_idx == children.size())
-			{
-				set_result(true);
-				return;
-			}
-			visit_child(next_child_idx);
 		}
+	private:
+		void on_enter();
+		void on_revisit();
+		friend class node_creator;
 	};
-	class random_sequence : public node
+	class always_seq :public sequence
 	{
+	protected:
+		always_seq(node* in_parent, node_idx_type in_node_idx, 
+			const btree_desc& in_btree, node_type in_type) :
+			sequence(in_parent, in_node_idx, in_btree, in_type)
+		{
+
+		}
+	private:
+		void on_revisit();
+		friend class node_creator;
+	};
+	class random_seq : public node
+	{
+	protected:
+		random_seq(node* in_parent, node_idx_type in_node_idx, 
+			const btree_desc& in_btree, node_type in_type) :
+			node(in_parent, in_node_idx, in_btree, in_type)
+		{
+
+		}
 	private:
 		std::vector<node_idx_type> _shuffle;
 		
-		void on_enter()
-		{
-			node::on_enter();
-			if (_shuffle.empty())
-			{
-				_shuffle.reserve(children.size());
-				for (int i = 0; i < children.size(); i++)
-				{
-					_shuffle.push_back(i);
-				}
-			}
-			std::shuffle(_shuffle.begin(), _shuffle.end(), _generator);
-			visit_child(_shuffle[0]);
-		}
-		void on_revisit()
-		{
-			node::on_revisit();
-			if (!children[_shuffle[next_child_idx]]->result)
-			{
-				set_result(false);
-				return;
-			}
-			next_child_idx += 1;
-			if (next_child_idx == children.size())
-			{
-				set_result(true);
-				return;
-			}
-			visit_child(_shuffle[next_child_idx]);
-		}
+		void on_enter();
+		void on_revisit();
+		friend class node_creator;
 
 	};
 	class select : public node
 	{
-		void on_enter()
+	protected:
+		select(node* in_parent, node_idx_type in_node_idx, 
+			const btree_desc& in_btree, node_type in_type) :
+			node(in_parent, in_node_idx, in_btree, in_type)
 		{
-			node::on_enter();
-			next_child_idx = 0;
-			visit_child(0);
+
 		}
-		void on_revisit()
-		{
-			node::on_revisit();
-			if (children[next_child_idx]->result)
-			{
-				set_result(true);
-				return;
-			}
-			next_child_idx += 1;
-			if (next_child_idx == children.size())
-			{
-				set_result(false);
-				return;
-			}
-			visit_child(next_child_idx);
-		}
+	private:
+		void on_enter();
+		void on_revisit();
+		friend class node_creator;
 	};
 	class probility : public node
 	{
+		probility(node* in_parent, node_idx_type in_node_idx, 
+			const btree_desc& in_btree, node_type in_type) :
+			node(in_parent, in_node_idx, in_btree, in_type)
+		{
+
+		}
 	private:
 		std::vector<std::uint32_t> _probilities;
 		
-		void on_enter()
-		{
-			node::on_enter();
-			if (_probilities.empty())
-			{
-				if (!init_prob_parameters())
-				{
-					_logger->warn("{} init_prob_parameters fail", debug_info());
-					_probilities.clear();
-					_agent->notify_stop();
-					return;
-				}
-			}
-			next_child_idx = prob_choose_child_idx();
-			visit_child(next_child_idx);
-
-
-		}
-		bool init_prob_parameters()
-		{
-			auto prob_iter = node_config.extra.find("prob");
-			if (prob_iter == node_config.extra.end())
-			{
-				return false;
-			}
-			if (!prob_iter->second.is_vector())
-			{
-				return false;
-			}
-			const meta::serialize::any_vector& prob_vec = std::get<meta::serialize::any_vector>(prob_iter->second);
-			if (prob_vec.size() != children.size())
-			{
-				return false;
-			}
-			for (auto& one_prob : prob_vec)
-			{
-				if (!one_prob.is_int())
-				{
-					return false;
-				}
-				_probilities.push_back(std::get<int>(one_prob));
-			}
-			return true;
-		}
-		node_idx_type prob_choose_child_idx() const
-		{
-			node_idx_type prob_sum = std::accumulate(_probilities.begin(), _probilities.end(), 0);
-			auto cur_choice = _distribution(_generator);
-			node_idx_type temp = cur_choice % prob_sum;
-			for (int i = 0; i < children.size(); i++)
-			{
-				temp -= _probilities[0];
-				if (temp <= 0)
-				{
-					return i;
-				}
-			}
-			return 0;
-
-		}
-		void on_revisit()
-		{
-			node::on_revisit();
-			set_result(children[next_child_idx]->result);
-		}
+		void on_enter();
+		bool init_prob_parameters();
+		node_idx_type prob_choose_child_idx() const;
+		void on_revisit();
+		friend class node_creator;
 
 	};
 	class if_else : public node
 	{
-		void on_enter()
+		if_else(node* in_parent, node_idx_type in_node_idx, 
+			const btree_desc& in_btree, node_type in_type) :
+			node(in_parent, in_node_idx, in_btree, in_type)
 		{
-			node::on_enter();
-			visit_child(0);
-		}
-		void on_revisit()
-		{
-			switch (next_child_idx)
-			{
-			case 0:
-				// the if node
-				if (children[0]->result)
-				{
-					next_child_idx = 1;
-				}
-				else
-				{
-					next_child_idx = 2;
-				}
-				visit_child(next_child_idx);
-				break;
-			case 1:
-				set_result(children[1]->result);
-				break;
-			case 2:
-				set_result(children[2]->result);
-				break;
-			default:
-				_logger->warn("{} invalid state visit if else node ", debug_info);
-				break;
-			}
 
 		}
+		void on_enter();
+		void on_revisit();
+		friend class node_creator;
 	};
 	class while_loop : public node
 	{
-		void on_enter()
+		while_loop(node* in_parent, node_idx_type in_node_idx, 
+			const btree_desc& in_btree, node_type in_type) :
+			node(in_parent, in_node_idx, in_btree, in_type)
 		{
-			node::on_enter();
-			visit_child(0);
+
 		}
-		void on_revisit()
-		{
-			switch (next_child_idx)
-			{
-			case 0:
-				// the if node
-				if (children[0]->result)
-				{
-					next_child_idx = 1;
-					visit_child(next_child_idx);
-				}
-				else
-				{
-					set_result(false);
-				}
-				break;
-			case 1:
-				next_child_idx = 0;
-				visit_child(next_child_idx);
-				break;
-			default:
-				_logger->warn("{} invalid state visit while_node node ", debug_info);
-				break;
-			}
-		}
+		void on_enter();
+		void on_revisit();
+		friend class node_creator;
 	};
 	class negative : public node
 	{
-		void on_enter()
+		negative(node* in_parent, node_idx_type in_node_idx, 
+			const btree_desc& in_btree, node_type in_type) :
+			node(in_parent, in_node_idx, in_btree, in_type)
 		{
-			node::on_enter();
-			visit_child(0);
+
 		}
-		void on_revisit()
-		{
-			node::on_revisit();
-			set_result(!children[0]->result);
-		}
+		void on_enter();
+		void on_revisit();
+		friend class node_creator;
 	};
-	class always : public node
+	class always_true : public node
 	{
-		void on_enter()
+		always_true(node* in_parent, node_idx_type in_node_idx, 
+			const btree_desc& in_btree, node_type in_type) :
+			node(in_parent, in_node_idx, in_btree, in_type)
 		{
-			node::on_enter();
-			visit_child(0);
+
 		}
-		void on_revisit()
-		{
-			node::on_revisit();
-			set_result(true);
-		}
+		void on_enter();
+		void on_revisit();
+		friend class node_creator;
 	};
 	class sub_tree : public node
 	{
-		void on_enter()
+		sub_tree(node* in_parent, node_idx_type in_node_idx, 
+			const btree_desc& in_btree, node_type in_type) :
+			node(in_parent, in_node_idx, in_btree, in_type)
 		{
-			node::on_enter();
-			if (children.empty())
-			{
-				if (!create_sub_tree_node())
-				{
-					_logger->warn("{} fail to load sub tree", debug_info());
-					_agent->notify_stop();
-					return;
-				}
-				
-			}
-			visit_child(0);
 
 		}
-		bool create_sub_tree_node()
-		{
-			auto sub_tree_iter = node_config.extra.find("sub_tree");
-			if (sub_tree_iter == node_config.extra.end())
-			{
-				return false;
-			}
-			if (!sub_tree_iter->second.is_str())
-			{
-				return false;
-			}
-			auto sub_tree_name = std::get<std::string>(sub_tree_iter->second);
-			const btree_desc* cur_tree_desc = btree_desc::load(sub_tree_name);
-			if (!cur_tree_desc)
-			{
-				return false;
-			}
-			auto new_root = create_node_by_idx(*cur_tree_desc, 0, this);
-			if (!new_root)
-			{
-				return false;
-			}
-			children.push_back(new_root);
-			return true;
-		}
-		void on_revisit()
-		{
-			set_result(children[0]->result);
-		}
+		void on_enter();
+		bool create_sub_tree_node();
+		void on_revisit();
+		friend class node_creator;
 
 	};
 
 	class parallel : public node
 	{
-		void on_enter()
+		parallel(node* in_parent, node_idx_type in_node_idx, 
+			const btree_desc& in_btree, node_type in_type) :
+			node(in_parent, in_node_idx, in_btree, in_type)
 		{
-			node::on_enter();
-			for (int i = 0; i < children.size(); i++)
-			{
-				visit_child(i);
-			}
+
 		}
-		void on_revisit()
-		{
-			bool final_result = false;
-			for (auto one_child : children)
-			{
-				if (one_child->_state == node_state::dead)
-				{
-					final_result = one_child->result;
-				}
-				one_child->interrupt();
-			}
-			
-			set_result(final_result);
-		}
+		void on_enter();
+		void on_revisit();
+		friend class node_creator;
 	};
 	class action : public node
 	{
+		action(node* in_parent, node_idx_type in_node_idx, 
+			const btree_desc& in_btree, node_type in_type) :
+			node(in_parent, in_node_idx, in_btree, in_type)
+		{
+
+		}
 		std::string action_name;
 		meta::serialize::any_vector action_args;
-		void on_enter()
+		void on_enter();
+		bool load_action_config();
+		friend class node_creator;
+	};
+	class wait_event : public node
+	{
+		wait_event(node* in_parent, node_idx_type in_node_idx,
+			const btree_desc& in_btree, node_type in_type) :
+			node(in_parent, in_node_idx, in_btree, in_type)
 		{
-			node::on_enter();
-			if (action_name.empty())
-			{
-				if (!load_action_config())
-				{
-					_logger->warn("{} fail to load action args with extra {}", debug_info(), encode(node_config.extra));
-					_agent->notify_stop();
-					return;
-				}
-			}
-			std::optional<bool> action_result = _agent->agent_action(this, action_name, action_args);
-			if (_agent->during_poll)
-			{
-				if (!action_result)
-				{
-					_state = node_state::blocking;
-					return;
-				}
-				else
-				{
-					set_result(action_result.value());
-				}
-			}
-			else
-			{
-				return;
-			}
-
-			
 
 		}
-		bool load_action_config()
+		friend class node_creator;
+	};
+	class reset : public node
+	{
+		reset(node* in_parent, node_idx_type in_node_idx,
+			const btree_desc& in_btree, node_type in_type) :
+			node(in_parent, in_node_idx, in_btree, in_type)
 		{
-			auto& extra = node_config.extra;
-			auto action_iter = extra.find("action_name");
-			if (action_iter == extra.end())
-			{
-				return false;
-			}
-			if (!action_iter->second.is_str())
-			{
-				return false;
-			}
-			action_name = std::get<std::string>(action_iter->second);
-			auto action_args_iter = extra.find("action_args");
-			if (action_args_iter == extra.end())
-			{
-				return false;
-			}
-			if (!action_args_iter->second.is_vector())
-			{
-				return false;
-			}
-			const meta::serialize::any_vector& args_vec = std::get<meta::serialize::any_vector>(action_iter->second);
-			for (auto& one_arg : args_vec)
-			{
-				if (!one_arg.is_vector())
-				{
-					return false;
-				}
-				auto& one_arg_vec = std::get<meta::serialize::any_vector>(one_arg);
-				if (one_arg_vec.size() != 2)
-				{
-					return false;
-				}
-				if (!one_arg_vec[0].is_str())
-				{
-					return false;
-				}
-				auto& cur_arg_type_str = std::get<std::string>(one_arg_vec[0]);
-				if (cur_arg_type_str == "blackboard")
-				{
-					if (!one_arg_vec[1].is_str())
-					{
-						return false;
-					}
-					auto& cur_bb_key = std::get<std::string>(one_arg_vec[1]);
-					auto cur_bb_iter = _agent->_blackboard.find(cur_bb_key);
-					if (cur_bb_iter == _agent->_blackboard.end())
-					{
-						return false;
-					}
-					action_args.push_back(cur_bb_iter->second);
-
-				}
-				else if (cur_arg_type_str == "plain")
-				{
-					action_args.push_back(one_arg_vec[1]);
-				}
-				else
-				{
-					return false;
-				}
-			}
-			return true;
 
 		}
+		friend class node_creator;
+	};
+	class node_creator
+	{
+	public:
+		static node* create_node_by_idx(const btree_desc& btree_config, 
+			node_idx_type node_idx, node* parent);
 	};
 }
